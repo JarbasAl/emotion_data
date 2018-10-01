796a3c353acb094a0c6d861a48bb6ee5d353f563
jarbasal <jarbasai@mailfence.com>
2018-10-01 06:20:05 +0100

    version up

--- emotion_data/__init__.py
+++ emotion_data/__init__.py
@@ -1,11 +1,32 @@
 from emotion_data.deepmoji import get_emojis, get_emotions
-from emotion_data.emotions import get_emotion, emotion_to_dimension, random_emotion
+from emotion_data.emotions import get_emotion, emotion_to_dimension, random_emotion, get_dimension
 from emotion_data.feelings import get_feeling, random_feeling
+from emotion_data.lexicons import get_emotion, get_sentiment, get_color, get_orientation, get_subjectivity
 
 
 class EmotionAnalyzer(object):
     def __init__(self):
         pass
+
+    @staticmethod
+    def get_emotion(word):
+        return get_emotion(word)
+
+    @staticmethod
+    def get_sentiment(word):
+        return get_sentiment(word)
+
+    @staticmethod
+    def get_color(word):
+        return get_color(word)
+
+    @staticmethod
+    def get_orientation(word):
+        return get_orientation(word)
+
+    @staticmethod
+    def get_subjectivity(word):
+        return get_subjectivity(word)
 
     @staticmethod
     def random_emotion():
@@ -24,9 +45,14 @@ class EmotionAnalyzer(object):
         return get_emojis(sentence)
 
     @staticmethod
-    def get_emotion(emotion):
-        return get_emotion(emotion)
+    def emotion(emotion_name):
+        return get_emotion(emotion_name)
 
     @staticmethod
-    def get_feeling(emotion):
-        return get_feeling(emotion)
+    def feeling(feeling_name):
+        return get_feeling(feeling_name)
+
+    @staticmethod
+    def dimension(dimension_name):
+        return get_dimension(dimension_name)
+
